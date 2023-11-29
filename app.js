bd7de217575e9a7e273275143e8e3a5ad4018d96
“Mariatu <mariatu.bah46@gmail.com>
2023-11-28 22:44:36 -0500

    updated site and added about me section as well as description of project 1

---
+++ app.js
@@ -0,0 +1,6 @@
+// Add any interactive features or form submission handling here
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+  event.preventDefault();
+  // Add code for form submission or validation
+  alert('Form submitted!');
+});
